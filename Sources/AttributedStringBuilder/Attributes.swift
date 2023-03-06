634f3ee6a0028b73610baed62020872fce2eb49b
Daniel Tull <dt@danieltull.co.uk>
2023-03-06 17:21:00 +0000

    Move Attributes to its own file

---
+++ Sources/AttributedStringBuilder/Attributes.swift
@@ -0,0 +1,33 @@
+
+import AppKit
+
+struct Attributes {
+    var family: String = "Helvetica"
+    var size: CGFloat = 14
+    var traits: NSFontTraitMask = []
+    var weight: Int = 5
+
+    var bold: Bool {
+        get {
+            traits.contains(.boldFontMask)
+        }
+        set {
+            if newValue {
+                traits.insert(.boldFontMask)
+            } else {
+                traits.remove(.boldFontMask)
+            }
+        }
+    }
+}
+
+extension Attributes {
+
+    var dictionary: [NSAttributedString.Key: Any] {
+        let fm = NSFontManager.shared
+        let font = fm.font(withFamily: family, traits: traits, weight: weight, size: size)
+        return [
+            .font: font as Any
+        ]
+    }
+}
--- Sources/AttributedStringBuilder/Environment.swift
+++ Sources/AttributedStringBuilder/Environment.swift
@@ -1,38 +1,6 @@
 
-import AppKit
 import Foundation
 
 struct Environment {
     var attributes = Attributes()
-}
-
-struct Attributes {
-    var family: String = "Helvetica"
-    var size: CGFloat = 14
-    var traits: NSFontTraitMask = []
-    var weight: Int = 5
-
-    var bold: Bool {
-        get {
-            traits.contains(.boldFontMask)
-        }
-        set {
-            if newValue {
-                traits.insert(.boldFontMask)
-            } else {
-                traits.remove(.boldFontMask)
-            }
-        }
-    }
-}
-
-extension Attributes {
-
-    var dictionary: [NSAttributedString.Key: Any] {
-        let fm = NSFontManager.shared
-        let font = fm.font(withFamily: family, traits: traits, weight: weight, size: size)
-        return [
-            .font: font as Any
-        ]
-    }
 }
