dc0fdd2b83e4d3bf94bf821366909040a50f35ec
Daniel Tull <dt@danieltull.co.uk>
2023-03-06 16:37:19 +0000

    Add AttributedStringBuilder

--- Sources/AttributedStringBuilder/AttributedStringBuilder.swift
+++ Sources/AttributedStringBuilder/AttributedStringBuilder.swift
@@ -1,3 +1,43 @@
 
+import Foundation
+
+@resultBuilder
 struct AttributedStringBuilder {
+
+    static func buildBlock(_ components: AttributedStringConvertible...) -> some AttributedStringConvertible {
+        Build { environment in
+            let result = NSMutableAttributedString()
+            for component in components {
+                let string = component.attributedString(environment: environment)
+                result.append(string)
+            }
+            return result
+        }
+    }
 }
+
+struct Build: AttributedStringConvertible {
+
+    let content: (Environment) -> NSAttributedString
+
+    func attributedString(environment: Environment) -> NSAttributedString {
+        content(environment)
+    }
+}
+
+#if DEBUG
+
+@AttributedStringBuilder
+var example: AttributedStringConvertible {
+    "Hello, World!"
+    "Another String"
+}
+
+import SwiftUI
+
+struct DebugPreview: PreviewProvider {
+    static var previews: some View {
+        Text(AttributedString(example.attributedString(environment: .init())))
+    }
+}
+#endif
