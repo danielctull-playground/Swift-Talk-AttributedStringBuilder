2757e70e5b963fc89785e7fc72a0e6b66de4d88f
Daniel Tull <dt@danieltull.co.uk>
2023-03-06 16:53:38 +0000

    Allow Lines to generate a single attributed string

--- Sources/AttributedStringBuilder/AttributedStringBuilder.swift
+++ Sources/AttributedStringBuilder/AttributedStringBuilder.swift
@@ -19,16 +19,22 @@ struct AttributedStringBuilder {
 
 struct Lines<Content: AttributedStringConvertible>: AttributedStringConvertible {
 
+    @AttributedStringBuilder
     let content: Content
+
     func attributedString(environment: Environment) -> [NSAttributedString] {
+        [single(environment: environment)]
+    }
+
+    func single(environment: Environment) -> NSAttributedString {
         let pieces = content.attributedString(environment: environment)
-        guard let first = pieces.first else { return [] }
+        guard let first = pieces.first else { return .init() }
         let result = NSMutableAttributedString(attributedString: first)
         for piece in pieces.dropFirst() {
             result.append(.init(string: "\n", attributes: environment.attributes))
             result.append(piece)
         }
-        return [result]
+        return result
     }
 }
 
@@ -53,7 +59,11 @@ import SwiftUI
 
 struct DebugPreview: PreviewProvider {
     static var previews: some View {
-        let string = Lines(content: example).attributedString(environment: .init())[0]
+
+        let string = Lines {
+            example
+        }.single(environment: .init())
+
         Text(AttributedString(string))
     }
 }
