ac3222294330ad2d59621a9fd610a110a1d88daa
Daniel Tull <dt@danieltull.co.uk>
2023-03-07 09:46:55 +0000

    Render a PDF version of the SwiftUI view

--- Sources/AttributedStringBuilder/SwiftUI.swift
+++ Sources/AttributedStringBuilder/SwiftUI.swift
@@ -11,6 +11,31 @@ struct Embed<Content: View>: AttributedStringConvertible {
     func attributedString(environment: Environment) -> [NSAttributedString] {
         let renderer = ImageRenderer(content: content)
         renderer.proposedSize = proposedSize
-        return renderer.nsImage!.attributedString(environment: environment)
+
+        let data = NSMutableData()
+
+        renderer.render { size, renderer in
+
+            var mediaBox = CGRect(origin: .zero, size: size)
+
+            guard
+                let consumer = CGDataConsumer(data: data),
+                let pdfContext = CGContext(consumer: consumer, mediaBox: &mediaBox, nil)
+            else {
+                return
+            }
+
+            pdfContext.beginPDFPage(nil)
+
+            pdfContext.translateBy(
+                x: mediaBox.size.width / 2 - size.width / 2,
+                y: mediaBox.size.height / 2 - size.height / 2)
+
+            renderer(pdfContext)
+            pdfContext.endPDFPage()
+            pdfContext.closePDF()
+        }
+
+        return NSImage(data: data as Data)!.attributedString(environment: environment)
     }
 }
